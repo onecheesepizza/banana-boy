d40476a0703a7f15f2fe993cf8a5ea2d14028601
onecheesepizza <52761974+onecheesepizza@users.noreply.github.com>
2020-06-30 22:50:46 -0700

    implement contact form using react

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,12 @@
 import React from "react";
+import ContactForm from "./ContactForm";
 
 function Contact(props) {
     return (
         <div className="contact">
         <div className="container">
             <h2>Contact</h2>
-            <form
-            action="https://formspree.io/mgenlpky"
-            method="POST"
-            >
-            <label>Name</label><br></br>
-                <input type="text" name="name"/>
-            <br></br>
-            <label>Email</label><br></br>
-                <input type="text" name="_replyto"/>
-            <br></br>
-            <label>Message:</label><br></br>
-                <textarea name="message"></textarea>
-            <br></br>
-            <button type="submit">Send</button>
-            </form>
+            <ContactForm></ContactForm>
         </div>
         </div>
     );
